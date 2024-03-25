9044c9f199ede8ebe7084115ea9aee90cc28796e
mishalzubair <160513180+mishal230@users.noreply.github.com>
2024-03-25 21:27:53 +0500

    created number guessing game

---
+++ index.js
@@ -0,0 +1,22 @@
+#! urs/bin/env code
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([{
+        message: `WELCOME TO THE GUESS GAME!
+Please Guess the number between 1-6:`,
+        type: "number",
+        name: "guessedNumber"
+    }
+]);
+if (answers.guessedNumber === randomNumber) {
+    console.log("Congratulations, you guess the right number!");
+}
+else if (answers.guessedNumber > randomNumber) {
+    console.log("Too high, Try Again!");
+}
+else if (answers.guessedNumber < randomNumber) {
+    console.log("Too low, Try Again!");
+}
+else {
+    console.log("Please enter the valid number!");
+}
