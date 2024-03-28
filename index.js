6f115235bc49f6a603a24598d8c25eacdb0b2986
mishalzubair <160513180+mishal230@users.noreply.github.com>
2024-03-28 12:08:58 +0500

    shebang created

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#! urs/bin/env code
+#! /usr/bin/env code
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 const answers = await inquirer.prompt([{
@@ -11,10 +11,10 @@ Please Guess the number between 1-6:`,
 if (answers.guessedNumber === randomNumber) {
     console.log("Congratulations, you guess the right number!");
 }
-else if (answers.guessedNumber > randomNumber) {
+else if (answers.guessedNumber >= randomNumber) {
     console.log("Too high, Try Again!");
 }
-else if (answers.guessedNumber < randomNumber) {
+else if (answers.guessedNumber <= randomNumber) {
     console.log("Too low, Try Again!");
 }
 else {
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-#! urs/bin/env code
+#! /usr/bin/env code
 import inquirer  from "inquirer";
 
 const randomNumber = Math.floor(Math.random()* 6 + 1);
@@ -12,9 +12,9 @@ Please Guess the number between 1-6:`,
 
 if(answers.guessedNumber === randomNumber){
     console.log("Congratulations, you guess the right number!");
-}else if(answers.guessedNumber > randomNumber){
+}else if(answers.guessedNumber >= randomNumber){
     console.log("Too high, Try Again!");
-}else if(answers.guessedNumber < randomNumber){
+}else if(answers.guessedNumber <= randomNumber){
     console.log("Too low, Try Again!");
 }else{
         console.log("Please enter the valid number!")
